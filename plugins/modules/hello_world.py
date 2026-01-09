b5a90989f26c497086a6f76f8bea950565f62e2f
asim10 <asimd1993@gmail.com>
2026-01-09 23:49:18 +0530

    updated custom module adinda.test.hello_world

--- plugins/modules/hello_world.py
+++ plugins/modules/hello_world.py
@@ -8,14 +8,48 @@
 # Create file as below
 # anugram/ciphertrust/plugins/modules/hello_world.py
 
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+DOCUMENTATION = r'''
+---
+module: test_hello_world
+
+short_description: Test Hello World module.
+
+description:
+  - print firstname lastname age profession in json format
+
+author: Asim Dinda (@asim10)
+version_added: "0.0.1"
+
+extends_documentation_fragment:
+  - community.elastic.login_options
+'''
+
+EXAMPLES = r'''
+---
+- name: Test hello_world module with valid data
+  hello_world:
+    firstname: "Adinda"
+    lastname: "Test"
+    age: 25
+    profession: "Engineer"
+  register: result
+- debug: var=result.user_data
+'''
+
+RETURN = r'''
+'''
+
 # import required Python libraries as well as required Ansible core modules
 
-import os
-import requests
-import urllib3
-import json
-import ast
-import re
+# import os
+# import requests
+# import urllib3
+# import json
+# import ast
+# import re
 
 from ansible.module_utils.basic import AnsibleModule
 
